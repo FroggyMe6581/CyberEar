50a290253e5fbcb5c01b887a2ee64daa9fb8e8e2
FroggyMe6581 <pwhall@email.arizona.edu>
2014-06-08 18:34:00 -0700

    Impulse Protection Button

--- gen/edu/engr498/cyberear/R.java
+++ gen/edu/engr498/cyberear/R.java
@@ -32,21 +32,22 @@ public final class R {
         public static final int right_sound=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080018;
+        public static final int action_settings=0x7f080019;
         public static final int button1=0x7f080005;
         public static final int button2=0x7f080004;
         public static final int button3=0x7f080013;
         public static final int button4=0x7f080014;
         public static final int button5=0x7f080011;
+        public static final int button6=0x7f080015;
         public static final int button_center=0x7f080010;
-        public static final int editText1=0x7f080016;
+        public static final int editText1=0x7f080017;
         public static final int imageView1=0x7f080001;
         public static final int imageView2=0x7f080009;
         public static final int imageView3=0x7f08000c;
         public static final int mrlayout=0x7f08000d;
         public static final int progressBar1=0x7f080003;
-        public static final int radioGroup1=0x7f080017;
-        public static final int rlSelectUser=0x7f080015;
+        public static final int radioGroup1=0x7f080018;
+        public static final int rlSelectUser=0x7f080016;
         public static final int seekBar_bal=0x7f08000f;
         public static final int seekBar_vol=0x7f08000e;
         public static final int textView1=0x7f080000;
@@ -73,7 +74,7 @@ public final class R {
         public static final int select_user=0x7f070004;
     }
     public static final class string {
-        public static final int abort=0x7f050025;
+        public static final int abort=0x7f050026;
         public static final int action_settings=0x7f050001;
         public static final int amp_dn=0x7f050011;
         public static final int amp_hint=0x7f05000b;
@@ -84,7 +85,7 @@ public final class R {
         public static final int balance_marker=0x7f050015;
         public static final int bass=0x7f050019;
         public static final int button_start=0x7f050007;
-        public static final int can_hear=0x7f050021;
+        public static final int can_hear=0x7f050022;
         public static final int center=0x7f050013;
         /** 
             <string name="config_button">Select Configuration\n\nor\n\nSet Up New Custom Configuration</string>
@@ -93,30 +94,31 @@ public final class R {
          */
         public static final int config_button=0x7f050003;
         public static final int end_activity_button=0x7f050017;
-        public static final int enter_name=0x7f05001e;
+        public static final int enter_name=0x7f05001f;
         /**  from HearingCheck by Seung 
          */
-        public static final int first_screen=0x7f05001c;
+        public static final int first_screen=0x7f05001d;
         public static final int freq_hint=0x7f050009;
-        public static final int hearing_check=0x7f05001f;
+        public static final int hearing_check=0x7f050020;
         public static final int hello_world=0x7f050006;
         public static final int hiss=0x7f05001a;
         /**   from Instruction by Seung 
          */
-        public static final int how_to_use=0x7f050027;
+        public static final int how_to_use=0x7f050028;
+        public static final int imp_prot_off=0x7f05001c;
         public static final int invite=0x7f050002;
         public static final int left_up=0x7f050012;
-        public static final int progress=0x7f050024;
+        public static final int progress=0x7f050025;
         public static final int revert=0x7f05001b;
         public static final int run_button=0x7f050004;
-        public static final int select_name=0x7f050023;
+        public static final int select_name=0x7f050024;
         public static final int set_amp=0x7f05000a;
         public static final int set_freq=0x7f050008;
-        public static final int start_button=0x7f05001d;
-        public static final int start_test=0x7f050020;
+        public static final int start_button=0x7f05001e;
+        public static final int start_test=0x7f050021;
         public static final int test_volume_text=0x7f050016;
-        public static final int title_activity_hearing_check=0x7f050022;
-        public static final int title_activity_instruction=0x7f050026;
+        public static final int title_activity_hearing_check=0x7f050023;
+        public static final int title_activity_instruction=0x7f050027;
         public static final int title_activity_mic_repeater=0x7f050005;
         public static final int title_activity_select_user=0x7f050018;
         public static final int vol_dn=0x7f05000f;
